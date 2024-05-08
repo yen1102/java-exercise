1a2b189425ede49c0961c84ba4ce8a3c2760f213
emtkenny <1225kenny1225@gmail.com>
2024-05-08 16:34:28 +0800

    Update JavaTest.java

--- JavaTest.java
+++ JavaTest.java
@@ -1,6 +1,7 @@
 public class JavaTest{
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
-        System.out.println("\"學號：\"01157109\"\n姓名：\"趙妍禎\"")
-        System.out.println("\"學號：\"01157124\"\n姓名：\"吳孟芸\"")
+        System.out.println("\"學號：\"01157109\"\n姓名：\"趙妍禎\"");
+        System.out.println("\"學號：\"01157124\"\n姓名：\"吳孟芸\"");
+        System.out.println("\"學號：\"01157137\"\n姓名：\"楊鈞凱\"");
         ;}}
