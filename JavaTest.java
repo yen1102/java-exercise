557e7b1685a07ddc8c973316d310d90d0c73d510
cha0yen <h45599641@gmail.com>
2024-05-08 16:02:27 +0800

    create

---
+++ JavaTest.java
@@ -0,0 +1,4 @@
+public class JavaTest{
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01157109\"\n姓名：\"趙妍禎\"");}}
